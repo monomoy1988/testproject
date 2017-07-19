672abb5077db9681c3a6a8757976a135a0612fbf
samsujj <samsujj@gmail.com>
2017-07-19 10:36:44 +0530

    hhhgg

---
+++ index-test.php
@@ -0,0 +1,30 @@
+<?php
+
+echo $_SERVER['REMOTE_ADDR'];
+
+?>
+<?php
+$url = json_decode(file_get_contents("http://api.ipinfodb.com/v3/ip-city/?key=f537f1df6074e0694de67c7eaa2dd9c371ed0f4b7ceb6db399d581a6455c4591&ip=".$_SERVER['REMOTE_ADDR']."&format=json"));
+
+//print the array to see the fields if you wish.
+//print_r($url);
+
+echo "<table border='1' width='50%' align='center'><tr><td>COUNTRY:</td><td>";
+echo $url->countryName;
+echo "</td></tr><tr><td>CITY:</td><td>";
+echo $url->cityName;
+echo "</td></tr><tr><td>STATE OR REGION:</td><td>";
+echo $url->regionName;
+echo "</td></tr><tr><td>IP ADDRESS:</td><td>";
+echo $url->ipAddress;
+echo "</td></tr><tr><td>COUNTRY CODE:</td><td>";
+echo $url->countryCode;
+echo "</td></tr><tr><td>LATITUTE:</td><td>";
+echo $url->latitude;
+echo "</td></tr><tr><td>LONGITUDE:</td><td>";
+echo $url->longitude;
+echo "</td></tr><tr><td>TIMEZONE:</td><td>";
+echo $url->timeZone;
+echo "</td></tr><tr></table>";
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <?php
-	//index.php 
-	//(c) 2011 Copyright All Rights Reserved: Apogee Design, Inc.
-	chdir(dirname(__FILE__));
-	require_once('includes/init.php');
-	if( AI_PAGE_NAME=='logoff' ) { require( ai_cascadepath('includes/logoff.php') ); die; }
-	else $AI->skin->drawpage();
+
+// change the following paths if necessary
+
+header('Content-type: text/html');
+header('Access-Control-Allow-Origin: http://torkq.com');  //I have also tried the * wildcard and get the same response
+//header('Access-Control-Allow-Origin: *');  //I have also tried the * wildcard and get the same response
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+
+// remove the following lines when in production mode
+defined('YII_MODE') or define('YII_MODE',true);
+defined('YII_DEBUG') or define('YII_DEBUG',false);
+if(YII_DEBUG)
+{
+
+    //$yii='../yii/framework/yii.php';
+}
+else
+{
+
+    //$yii='yii/framework/yii.php';
+}
+if(YII_MODE)
+{
+
+    $yii='yii/framework/yii.php';
+    $config=dirname(__FILE__).'/protected/config/main-server.php';
+
+
+}
+else
+{
+    $yii='../yii/framework/yii.php';
+    $config=dirname(__FILE__).'/protected/config/main.php';
+}
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once($yii);
+
+Yii::createWebApplication($config)->run();
 
