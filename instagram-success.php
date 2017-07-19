b6096301427ab6f34c187448537d21d67fd8f404
samsujj <samsujj@gmail.com>
2017-07-19 10:34:24 +0530

    hhh

---
+++ instagram-success.php
@@ -0,0 +1,37 @@
+<?php
+require 'Instagram.php';
+
+use MetzWeb\Instagram\Instagram;
+
+$instagram = new Instagram(array(
+    'apiKey' => 'a41171a75a0346e4916608a020241ea2',
+    'apiSecret' => 'a9c8c31b75e84fea9d3fe16c5f7f4b1e',
+    'apiCallback' => 'http://torqkd.com/instagram-success.php' // must point to success.php
+));
+
+
+// receive OAuth code parameter
+$code = $_GET['code'];
+// check whether the user has granted access
+if (isset($code)) {
+    // receive OAuth token object
+    $data = $instagram->getOAuthToken($code);
+    $username = $data->user->username;
+    // store user access token
+    $instagram->setAccessToken($data);
+    // now you have access to all authenticated user methods
+    $result = $instagram->getUserMedia();
+
+    var_dump($data);
+    var_dump($username);
+
+} else {
+    // check whether an error occurred
+    if (isset($_GET['error'])) {
+        echo 'An error occurred: ' . $_GET['error_description'];
+    }
+
+    echo 'dsfs';
+}
+
+?>
